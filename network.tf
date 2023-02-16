b2c6562c2c719a91aa8f17adcba48ddec0397d36
Nassima Benchernine <bench_nas@hotmail.com>
2023-02-16 20:34:11 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/Nassima_Benchernine_Training/network/azurerm"
   version = "3.5.0"
-  ressouce_group_name = azurerm_resource_group.myresourcegroup.name
+  resouce_group_name = azurerm_resource_group.myresourcegroup.name
 }
